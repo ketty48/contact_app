12a325f8aa60bd8c88c8217fc878a2aa8a432f75
Magnifique UMURERWA <ketymagnifique@gmail.com>
2024-04-02 11:04:20 +0200

    routers

--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -1,1 +1,15 @@
-// Contact routes here
+// Contact router
+import express from 'express'
+const router =express.Router()
+import contactControllers from '../controllers/contact.controller.js'
+
+router.route('/search').get(contactControllers.getContactByEmailOrByPhone);
+// router.route('/search').get(contactControllers.getContactByPhone)
+router.route('/').post(contactControllers.createContact).get(contactControllers.getAllContact)
+router.route('/:id').get(contactControllers.getContact).put(contactControllers.updateContact).delete(contactControllers.deleteContact)
+
+
+
+
+export default router
+
