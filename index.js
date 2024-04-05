e6dde7cb0883b8fa34a4a34afc79fff08aa2ee00
Magnifique UMURERWA <ketymagnifique@gmail.com>
2024-04-05 11:22:06 +0200

    updates

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./docs/swagger.json" assert { type: "json" };
+
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
@@ -5,13 +8,15 @@ const app = express();
 import configuration from './configs/index.js';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import contactRouter from './routes/contact.routes.js'
-import notFound from './middlewares/notFound.js'
+import contactRouter from './routes/contact.routes.js';
+import notFound from './middlewares/notFound.js';
 
 app.use(express.json());
 app.use(cors());
-app.use('/contact/api',contactRouter)
-app.use(notFound)
+app.use('/contact',contactRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); // Use swaggerDocument instead of swagger
+
+// app.use(notFound);
 mongoose.connect(configuration.mongoURI)
 .then(() => {
     app.listen(configuration.port, ()=> {
@@ -21,3 +26,6 @@ mongoose.connect(configuration.mongoURI)
 .catch(err => {
     console.log(err);
 });
+app.use("/",(req,res)=>{
+    res.send("welcome");
+})
